af70141294f50f90db3e2d8ddfe837f0e21ca3ad
Ian Enders <ian.enders@gmail.com>
2017-12-02 00:20:00 -0500

    Solution for 2

---
+++ 2017/02/solution.rb
@@ -0,0 +1,35 @@
+def diff(table)
+  total = 0
+  table.each do |row|
+    total += row.max - row.min
+  end
+  total
+end
+
+def divisors(row)
+  vals = row.sort
+  while true
+    val = vals.shift
+    vals.each do |num|
+      return [ num, val ] if num % val == 0
+    end
+  end
+end
+
+def divide(table)
+  total = 0
+  table.each do |row|
+    vals = divisors(row)
+    total += vals[0] / vals[1]
+  end
+  total
+end
+
+table = []
+File.read('input.txt').each_line do |line|
+  table << line.split(/\s+/).map(&:to_i)  
+end
+
+puts 'Part 1', diff(table)
+
+puts 'Part 2', divide(table)
