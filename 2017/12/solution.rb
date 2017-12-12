de882a2d0e51060c95bfce00755fd5712e006e5a
Ian Enders <ian.enders@gmail.com>
2017-12-12 11:00:42 -0500

    Fix solution for 2017-12

--- 2017/12/solution.rb
+++ 2017/12/solution.rb
@@ -1,36 +1,36 @@
+def load_programs
+  programs = {}
+  File.read('input.txt').each_line do |line|
+    program, nodes = line.chomp.split(' <-> ')
+    programs[program.to_i] = nodes.split(', ').map(&:to_i)
+  end
+  programs
+end
+
 def group(programs, target)
   in_target = {}
-  in_target[target] = true
-  to_expand = programs[target]
+  to_expand = [ target ]
   while to_expand.size > 0 do
     target = to_expand.shift
     in_target[target] = true
-    # TODO: Fix this. My input had a bug (I believe).
-    # If I strip all keys from a program out after constructing a group
-    # I get a nil pointer when trying to construct the group for 889.
-    # 889 <-> 0, 1494, since I was not able to reach it from 0 in first pass.
-    # This solution gives the incorrect answer (which is off by 1).
     to_expand += programs[target].reject {|i| in_target[i] }
   end
-  in_target.keys.sort
+  in_target.keys
 end
 
 def groups(programs)
   groups = []
-  keys = programs.keys.sort
-  while keys.size > 0 do
-    set = group(programs, keys.first)
-    keys -= set
+  while programs.size > 0 do
+    set = group(programs, programs.keys.first)
+    set.each do |key|
+      programs.delete(key)
+    end
     groups.push(set)
   end
   groups
 end
 
-programs = {}
-File.read('input.txt').each_line do |line|
-  program, nodes = line.chomp.split(' <-> ')
-  programs[program.to_i] = nodes.split(', ').map(&:to_i)
-end
+programs = load_programs
 
 puts "Part 1: #{group(programs, 0).size}"
 puts "Part 2: #{groups(programs).size}"
