e901540b279065be9f693ca88b5a526eaf847635
Ian Enders <ian.enders@gmail.com>
2020-12-20 19:51:00 -0500

    Day 11 Part 2

--- 2020/src/main/kotlin/adventofcode/Day11Solution.kt
+++ 2020/src/main/kotlin/adventofcode/Day11Solution.kt
@@ -2,65 +2,108 @@ package adventofcode
 
 class Day11Solution : Solution(11) {
 
-    private var layout: List<List<Boolean?>> = inputLines.map {
-        it.map { position -> if (position == 'L') false else null }
-    }
+    private abstract class Simulator(
+        protected var layout: List<List<Boolean?>>,
+        private val emptyTolerance: Int
+    ) {
 
-    private fun directAdjacencyCount(x: Int, y: Int): Int =
-        listOf(
-            layout.getOrNull(x - 1)?.getOrNull(y - 1),
-            layout.getOrNull(x - 1)?.getOrNull(y),
-            layout.getOrNull(x - 1)?.getOrNull(y + 1),
-            layout.getOrNull(x)?.getOrNull(y - 1),
-            layout.getOrNull(x)?.getOrNull(y + 1),
-            layout.getOrNull(x + 1)?.getOrNull(y - 1),
-            layout.getOrNull(x + 1)?.getOrNull(y),
-            layout.getOrNull(x + 1)?.getOrNull(y + 1)
-        ).count { it == true }
+        protected abstract fun valueAt(x: Int, y: Int, dir: Pair<Int, Int>): Boolean
 
-    private fun round(): Boolean {
-        var changed = false
+        protected val adjacencyDirs: List<Pair<Int, Int>> =
+            (-1..1).map { x ->
+                (-1..1).mapNotNull { y ->
+                    if (x == 0 && y == 0) null
+                    else Pair(x, y)
+                }
+            }.flatten()
 
-        val next = mutableListOf<List<Boolean?>>()
+        private fun count(x: Int, y: Int): Int =
+            adjacencyDirs.count { valueAt(x, y, it) }
 
-        layout.forEachIndexed { i, row ->
-            val nextRow = row.toMutableList()
-            row.forEachIndexed { j, col ->
-                when (col) {
-                    false -> {
-                        if (directAdjacencyCount(i, j) == 0) {
-                            nextRow[j] = true
-                            changed = true
+        private fun round(): Boolean {
+            var changed = false
+
+            val next = mutableListOf<List<Boolean?>>()
+
+            layout.forEachIndexed { i, row ->
+                val nextRow = row.toMutableList()
+                row.forEachIndexed { j, col ->
+                    when (col) {
+                        false -> {
+                            if (count(i, j) == 0) {
+                                nextRow[j] = true
+                                changed = true
+                            }
                         }
-                    }
-                    true -> {
-                        if (directAdjacencyCount(i, j) >= 4) {
-                            nextRow[j] = false
-                            changed = true
+                        true -> {
+                            if (count(i, j) >= emptyTolerance) {
+                                nextRow[j] = false
+                                changed = true
+                            }
                         }
                     }
                 }
+                next.add(nextRow)
             }
-            next.add(nextRow)
+
+            if (changed) {
+                layout = next.toList()
+            }
+
+            return changed
         }
 
-        if (changed) {
-            layout = next.toList()
+        private val occupiedSeats: Int
+            get() = layout.sumBy { row ->
+                row.count { col -> col == true }
+            }
+
+        fun simulateOccupiedSeats(): Int {
+            while (round()) {}
+            return occupiedSeats
         }
 
-        return changed
     }
 
-    private val occupiedSeats: Int
-        get() = layout.sumBy { row ->
-            row.count { col -> col == true }
-        }
+    private class DirectAdjacencySimulator(
+        layout: List<List<Boolean?>>
+    ) : Simulator(layout, emptyTolerance = 4) {
 
-    override fun part1(): String {
-        while (round()) {}
-        return occupiedSeats.toString()
+        override fun valueAt(x: Int, y: Int, dir: Pair<Int, Int>): Boolean =
+            layout.getOrNull(x + dir.first)?.getOrNull(y + dir.second) == true
+
     }
 
-    override fun part2() = "TODO"
+    private class RayBasedAdjacencySimulator(
+        layout: List<List<Boolean?>>
+    ) : Simulator(layout, emptyTolerance = 5) {
+
+        override fun valueAt(x: Int, y: Int, dir: Pair<Int, Int>): Boolean {
+            var currX = x
+            var currY = y
+            val maxX = layout.size
+            val maxY = layout[0].size
+            do {
+                currX += dir.first
+                currY += dir.second
+                val value = layout.getOrNull(currX)?.getOrNull(currY)
+                if (value != null) {
+                    return value
+                }
+            } while (currX in 0..maxX && currY in 0..maxY)
+            return false
+        }
+
+    }
+
+    private val layout: List<List<Boolean?>> = inputLines.map {
+        it.map { position -> if (position == 'L') false else null }
+    }
+
+    override fun part1(): String =
+        DirectAdjacencySimulator(layout).simulateOccupiedSeats().toString()
+
+    override fun part2(): String =
+        RayBasedAdjacencySimulator(layout).simulateOccupiedSeats().toString()
 
 }
