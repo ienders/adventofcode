225f35741a9576b96d30c27bf0b4d16297761348
Ian Enders <ian.enders@gmail.com>
2021-12-14 21:11:09 -0500

    2021 - C#

---
+++ 2021/AOC/AdventOfCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode {
+    internal class Program {
+        public static void Main(string[] args) {
+            Console.WriteLine();
+            Console.WriteLine("==============================");
+            Console.WriteLine("Welcome to Advent of Code 2021");
+            Console.WriteLine("==============================");
+            Console.WriteLine();
+
+            var day = args.FirstOrDefault();
+            if (day == null) {
+                Console.WriteLine("Provide day.");
+                return;
+            }
+
+            var solution = short.Parse(day) switch {
+                1 => new Day1Solution(),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+
+            Console.WriteLine("Part 1: {0}", solution.Part1());
+            Console.WriteLine("Part 2: {0}", solution.Part2());
+        }
+    }
+}
