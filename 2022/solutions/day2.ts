8f5cdd434a674aba6cde51c5c4e5aa56b1e92efd
Ian Enders <ian.enders@gmail.com>
2022-12-13 21:30:53 -0500

    2022 Day 2 part 2 - with refactor

--- 2022/solutions/day2.ts
+++ 2022/solutions/day2.ts
@@ -1,21 +1,78 @@
 import { Solution } from '../lib/solution'
 import { getInput } from '../lib/input'
 
-type Input = [
-    'A' | 'B' | 'C',
-    'X' | 'Y' | 'Z'
-]
+enum Move {
+  Rock = 'A',
+  Paper = 'B',
+  Scissors = 'C'
+}
 
-const tiesAgainst = { 'X': 'A', 'Y': 'B', 'Z': 'C' }
-const winsAgainst = { 'X': 'C', 'Y': 'A', 'Z': 'B' }
+enum Result {
+  Win = 'X',
+  Draw = 'Y',
+  Loss = 'Z'
+}
 
-const result = (input: Input): number => {
-  if (winsAgainst[input[1]] === input[0]) return 6
-  if (tiesAgainst[input[1]] === input[0]) return 3
+type Input = [ Move, Result ]
+type Match = [ Move, Move ]
+
+const beats = (move: Move): Move => {
+  switch (move) {
+    case Move.Rock: return Move.Paper
+    case Move.Paper: return Move.Scissors
+    case Move.Scissors: return Move.Rock
+  }
+}
+
+const losesTo = (move: Move): Move => {
+  switch (move) {
+    case Move.Rock: return Move.Scissors
+    case Move.Paper: return Move.Rock
+    case Move.Scissors: return Move.Paper
+  }
+}
+
+const score = (move: Move): number => {
+  switch (move) {
+    case Move.Rock: return 1
+    case Move.Paper: return 2
+    case Move.Scissors: return 3
+  }
+}
+
+const inputToMatch = (input: Input, computeMove: (input: Input) => Move): Match =>
+  [ input[0], computeMove(input) ]
+
+const part1ComputeMove = (input: Input): Move => {
+  const [ , result ] = input
+  switch (result) {
+    case Result.Win: return Move.Rock
+    case Result.Draw: return Move.Paper
+    case Result.Loss: return Move.Scissors
+  }
+}
+
+const part2ComputeMove = (input: Input): Move => {
+  const [ move, result ] = input
+  switch (result) {
+    case Result.Win: return losesTo(move)
+    case Result.Draw: return move
+    case Result.Loss: return beats(move)
+  }
+}
+  
+const matchScore = (match: Match): number => {
+  const [ move, playerMove ] = match
+  if (playerMove === move) return 3
+  if (playerMove === beats(move)) return 6
   return 0
 }
 
-const score = (input: Input): number => ({ 'X': 1, 'Y': 2, 'Z': 3 }[input[1]])
+const totalScore = (matches: Match[]): number =>
+  matches.reduce((acc, match) =>
+    acc + score(match[1]) + matchScore(match),
+    0
+  )
 
 export default async function (session: string): Promise<Solution> {
   const inputs: Input[] = (await getInput(session, 2)).map((row) => {
@@ -23,7 +80,7 @@ export default async function (session: string): Promise<Solution> {
     return [ parts[0], parts[1] ] as Input
   })
   return {
-    part1: inputs.reduce((acc, input) => acc + score(input) + result(input), 0),
-    part2: 'TODO'
+    part1: totalScore(inputs.map((input) => inputToMatch(input, part1ComputeMove))),
+    part2: totalScore(inputs.map((input) => inputToMatch(input, part2ComputeMove)))
   }
 }
